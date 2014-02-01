9378efd87ae536d09d54756f55b6f1405a238aa2
mohant1 <mohant1@SNB44Y9S1.michigan.org>
2014-02-01 10:33:09 -0500

    Added detents.

--- src/com/team1504/robot2014/Mecanum.java
+++ src/com/team1504/robot2014/Mecanum.java
@@ -6,6 +6,8 @@
 
 package com.team1504.robot2014;
 
+import com.sun.squawk.util.MathUtils;
+
 
 /**
  *
@@ -17,9 +19,15 @@ public class Mecanum
     private double back_left_val;
     private double front_right_val;
     private double back_right_val;
-    
+    double correct_multiplier;
     double rotation_offset;
+    
+    public Mecanum()
+    {
+        correct_multiplier = 1;
         
+    }
+    
     public void front_rotation(double rot_offset)
     {
        rotation_offset = rot_offset;
@@ -29,19 +37,26 @@ public class Mecanum
     {
         double forward = directions[0];
         double right = directions[1];
-        double counter_clockwise = directions[2];
+        double ccw = directions[2];
         
-        double max = Math.max(1.0, Math.abs(forward) + Math.abs(right) + Math.abs(counter_clockwise));
+        double theta = MathUtils.atan2(forward, right);
+        double dx = correct_x(theta) * distance(right, forward) * correct_multiplier;
+        double dy = correct_y(theta) * distance(right, forward) * correct_multiplier;
+        
+        double forward_sans_offset = MathUtils.pow(forward, 3) + dy;
+        double right_sans_offset = MathUtils.pow(right, 3) + dx;
+        
+        double max = Math.max(1.0, Math.abs(forward) + Math.abs(right) + Math.abs(ccw));
 //        System.out.println(max);    
         
 //        System.out.println(forward + " " + right + " " + counter_clockwise);
-        forward = forward * Math.cos(rotation_offset) + right * Math.sin(rotation_offset);
-        right = right * Math.cos(rotation_offset) - forward * Math.sin(rotation_offset);
+        forward = forward_sans_offset * Math.cos(rotation_offset) + right * Math.sin(rotation_offset);
+        right = right_sans_offset * Math.cos(rotation_offset) - forward * Math.sin(rotation_offset);
         
-        front_left_val = ((forward + right - counter_clockwise) * RobotMap.FRONT_LEFT_MAGIC_NUMBER / max);        
-        back_left_val = ((forward - right - counter_clockwise) * RobotMap.BACK_LEFT_MAGIC_NUMBER / max);
-        back_right_val = ((forward + right + counter_clockwise) * RobotMap.BACK_RIGHT_MAGIC_NUMBER / max);
-        front_right_val = ((forward - right + counter_clockwise) * RobotMap.FRONT_RIGHT_MAGIC_NUMBER / max);
+        front_left_val = ((forward + right - ccw) * RobotMap.FRONT_LEFT_MAGIC_NUMBER / max);        
+        back_left_val = ((forward - right - ccw) * RobotMap.BACK_LEFT_MAGIC_NUMBER / max);
+        back_right_val = ((forward + right + ccw) * RobotMap.BACK_RIGHT_MAGIC_NUMBER / max);
+        front_right_val = ((forward - right + ccw) * RobotMap.FRONT_RIGHT_MAGIC_NUMBER / max);
         
         System.out.println(front_left_val + " " + back_left_val + " " + back_right_val + " " + front_right_val);
     }
@@ -85,4 +100,21 @@ public class Mecanum
        // double max = Math.max(Math.abs(first), Math.abs(second));
         
    }
+    public void set_correct_multiplier(double correct_multiplier)
+    {
+        this.correct_multiplier = correct_multiplier;
+    }
+    
+    public static double distance(double x, double y)
+    {
+        return Math.sqrt(x*x + y*y);
+    }
+    private double correct_x(double theta)
+    {
+       return -Math.sin(theta) * (-Math.sin(8*theta) - 0.25 * Math.sin(4*theta)); 
+    }
+    private double correct_y(double theta)
+    {
+       return Math.cos(theta) * (-Math.sin(8*theta) - 0.25 * Math.sin(4*theta)); 
+    }
 }
