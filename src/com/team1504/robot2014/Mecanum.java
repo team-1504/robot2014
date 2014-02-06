c23284524a9584168a9b74f6df4d9709531f932d
mohant1 <mohant1@SNB44Y9S1.michigan.org>
2014-02-06 18:13:02 -0500

    did the thing guys

--- src/com/team1504/robot2014/Mecanum.java
+++ src/com/team1504/robot2014/Mecanum.java
@@ -21,13 +21,13 @@ public class Mecanum
     private double back_right_val;
     double correct_multiplier;
     double rotation_offset;
+    double[] orbit_offset = new double[2];
     
     public Mecanum()
     {
         correct_multiplier = 1;
         
     }
-    
     public void front_rotation(double rot_offset)
     {
        rotation_offset = rot_offset;
@@ -38,20 +38,17 @@ public class Mecanum
         double forward = directions[0];
         double right = directions[1];
         double ccw = directions[2];
-        
-        double theta = MathUtils.atan2(forward, right);
-        double dx = correct_x(theta) * distance(right, forward) * correct_multiplier;
-        double dy = correct_y(theta) * distance(right, forward) * correct_multiplier;
-        
-        double forward_sans_offset = MathUtils.pow(forward, 3) + dy;
-        double right_sans_offset = MathUtils.pow(right, 3) + dx;
-        
+           
         double max = Math.max(1.0, Math.abs(forward) + Math.abs(right) + Math.abs(ccw));
 //        System.out.println(max);    
         
 //        System.out.println(forward + " " + right + " " + counter_clockwise);
-        forward = forward_sans_offset * Math.cos(rotation_offset) + right * Math.sin(rotation_offset);
-        right = right_sans_offset * Math.cos(rotation_offset) - forward * Math.sin(rotation_offset);
+        directions = detents(directions);
+        
+        directions = front_side(directions);
+        
+        directions = orbit_point(directions);
+        
         
         front_left_val = ((forward + right - ccw) * RobotMap.FRONT_LEFT_MAGIC_NUMBER / max);        
         back_left_val = ((forward - right - ccw) * RobotMap.BACK_LEFT_MAGIC_NUMBER / max);
@@ -59,6 +56,32 @@ public class Mecanum
         front_right_val = ((forward - right + ccw) * RobotMap.FRONT_RIGHT_MAGIC_NUMBER / max);
         
         System.out.println(front_left_val + " " + back_left_val + " " + back_right_val + " " + front_right_val);
+    }
+    
+    private double[] detents(double[] dircn)
+    {
+        double theta = MathUtils.atan2(dircn[0], dircn[1]);
+        double dx = correct_x(theta) * distance(dircn[1], dircn[0]) * correct_multiplier;
+        double dy = correct_y(theta) * distance(dircn[1], dircn[0]) * correct_multiplier;
+        
+        dircn[0] = MathUtils.pow(dircn[0], 3) + dy;
+        dircn[1] = MathUtils.pow(dircn[1], 3) + dx;
+        
+        return dircn;
+
+    }
+    private double[] front_side(double[] dircn)
+    {
+        double[] dir_offset = new double[3];
+        dir_offset[0] = dircn[0] * Math.cos(rotation_offset) + dircn[1] * Math.sin(rotation_offset);
+        dir_offset[1] = dircn[1] * Math.cos(rotation_offset) + dircn[0] * Math.sin(rotation_offset);
+        dir_offset[2] = dircn[2];
+        return dir_offset;
+    }
+    
+    private double[] orbit_point(double[] dircn)
+    {
+        //do the thing guise
     }
 
     public double get_front_left()
@@ -78,28 +101,6 @@ public class Mecanum
         return front_right_val;
     }
 
-    
-    public void tankcanum_drive(double left_y, double left_x, double right_y, double right_x)
-    {
-       // double ccw = (right_y - left_y)/2;
-       // double forward = (left_y + right_y)/2;
-       // double right = (left_x + right_x)/2;
-       // mecanum_drive(forward, right, ccw);
-        
-        double max = Math.max(1.0, Math.abs(left_y) + Math.abs(left_x) + Math.abs((right_y - left_y)/2));
-       
-        front_left_val = ((left_y + left_x + ((right_y - left_y)/2)) / max);
-        back_left_val = ((left_y - left_x - ((right_y - left_y)/2)) / max);
-        back_right_val = (((left_y - left_x + ((right_y - left_y)/2)) * -1) / max);
-        front_right_val = (((left_y + left_x + ((right_y - left_y)/2)) * -1) / max);
-        
-        
-
-        //double first = Math.max(Math.abs(fl_setx), Math.abs(bl_setx));
-        //double second = Math.max(Math.abs(fr_setx), Math.abs(br_setx));
-       // double max = Math.max(Math.abs(first), Math.abs(second));
-        
-   }
     public void set_correct_multiplier(double correct_multiplier)
     {
         this.correct_multiplier = correct_multiplier;
@@ -117,4 +118,25 @@ public class Mecanum
     {
        return Math.cos(theta) * (-Math.sin(8*theta) - 0.25 * Math.sin(4*theta)); 
     }
+//    public void tankcanum_drive(double left_y, double left_x, double right_y, double right_x)
+//    {
+//       // double ccw = (right_y - left_y)/2;
+//       // double forward = (left_y + right_y)/2;
+//       // double right = (left_x + right_x)/2;
+//       // mecanum_drive(forward, right, ccw);
+//        
+//        double max = Math.max(1.0, Math.abs(left_y) + Math.abs(left_x) + Math.abs((right_y - left_y)/2));
+//       
+//        front_left_val = ((left_y + left_x + ((right_y - left_y)/2)) / max);
+//        back_left_val = ((left_y - left_x - ((right_y - left_y)/2)) / max);
+//        back_right_val = (((left_y - left_x + ((right_y - left_y)/2)) * -1) / max);
+//        front_right_val = (((left_y + left_x + ((right_y - left_y)/2)) * -1) / max);
+//        
+//        
+//
+//        //double first = Math.max(Math.abs(fl_setx), Math.abs(bl_setx));
+//        //double second = Math.max(Math.abs(fr_setx), Math.abs(br_setx));
+//       // double max = Math.max(Math.abs(first), Math.abs(second));
+//        
+//   }
 }
